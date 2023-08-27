58c196f08ba6eb1564e6e036a89882a191a816ad
Aniket Biswas <74890960+aniket-biswas@users.noreply.github.com>
2023-08-27 12:30:26 +0530

    Update Program - 4- PrimeCompositeNo.c
    Removed one function

--- Week 3 - 21.08.2023/Program - 4- PrimeCompositeNo.c
+++ Week 3 - 21.08.2023/Program - 4- PrimeCompositeNo.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-
-// Function to check if a number is prime
-int isPrime(int num) {
-    if (num <= 1) {
-        return 0; // 1 is neither prime nor composite
-    }
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            return 0; // Not prime
-        }
-    }
-    return 1; // Prime
-}
+#include <math.h>
 
 int main() {
     int num, primeCount = 0, compositeCount = 0;
 
-    printf("Enter numbers. \nEnter -1 to stop.\n");
+    printf("Enter numbers.\n");
+    printf("Enter -1 to stop.\n");
 
     do {
         printf("Enter a number: ");
@@ -26,20 +15,34 @@ int main() {
             break;
         }
 
-        if (isPrime(num)) {
-            printf("%d is prime.\n", num);
-            primeCount++;
-        } else if (num > 1) {
-            printf("%d is composite.\n", num);
-            compositeCount++;
+        if (num <= 1) {
+            // 1 is neither prime nor composite
+        } else {
+            int isPrime = 1;
+            for (int i = 2; i * i <= num; ++i) {
+                if (num % i == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+            if (isPrime) {
+                printf("%d is prime.\n", num);
+                primeCount++;
+            } else {
+                printf("%d is composite.\n", num);
+                compositeCount++;
+            }
         }
-    } while (1); // Keep looping until -1 is entered
+    } while (1);
 
     printf("Total prime numbers: %d\n", primeCount);
     printf("Total composite numbers: %d\n", compositeCount);
 
     return 0;
 }
+
+
+
 
 // Algorithm:
 
@@ -52,14 +55,17 @@ int main() {
 //    2. Read num
 //    3. If num is equal to -1, go to step 6
 //    4. If num is less than or equal to 1, go to step 5.1
-//    5. If isPrime(num) returns true, go to step 5.2
-//    6. Else, go to step 5.3
+//    5. Set isPrime to 1
+//    6. Loop i from 2 to the square root of num:
+//       1. If num is divisible by i, set isPrime to 0 and break the loop
+//    7. If isPrime is true, go to step 5.2
+//    8. Else, go to step 5.3
+// 5.1: Skip the rest of the loop and go to step 5
+// 5.2: Print "num is prime.", increment primeCount, and go to step 5
+// 5.3: Print "num is composite.", increment compositeCount, and go to step 5
 // 6. Print "Total prime numbers:", primeCount
 // 7. Print "Total composite numbers:", compositeCount
 // 8. End
 
-// Function isPrime(num):
-// 1. If num is less than or equal to 1, return 0
-// 2. Loop i from 2 to the square root of num:
-//    1. If num is divisible by i, return 0
-// 3. Return 1
+
+
